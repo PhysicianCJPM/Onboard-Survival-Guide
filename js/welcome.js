7fb50c69f5fe071a0f73a957647121f5b1af6608
Calee Mangunay <c.mangunay@mec.ph>
2026-01-28 16:42:58 +0800

    added layer style

--- js/welcome.js
+++ js/welcome.js
@@ -2,15 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const character = document.querySelector('.character');
 
     character.addEventListener('click', () => {
-        // Start twirl
         character.classList.add('twirl');
 
-        // After twirl ends, remove twirl & add bounce
         character.addEventListener('animationend', () => {
             character.classList.remove('twirl');
             character.classList.add('bounce');
 
-            // Remove bounce after it ends so it can repeat on next click
             character.addEventListener('animationend', () => {
                 character.classList.remove('bounce');
             }, { once: true });
